0785b3fab405788127b3c29dac543c40d86acace
Jim Fulton <jim@zope.com>
2013-08-11 10:57:06 -0400

    Change to class from singleton to make testing easier, by allowing a mocked client to be passed in.
    Include bucket name in state to simplify API.

--- src/main/scala/S3.scala
+++ src/main/scala/S3.scala
@@ -1,20 +1,24 @@
 package com.zope.s3blobserver
 
+import com.amazonaws.services.s3.AmazonS3Client
 import java.io.File
 
 object S3 {
-  val client = new com.amazonaws.services.s3.AmazonS3Client(
+  def default_client = new AmazonS3Client(
     new com.amazonaws.auth.DefaultAWSCredentialsProviderChain())
+}
 
-  def get(bucket: String, key: String, dest: File): Unit = {
+class S3(client: AmazonS3Client, bucket: String) {
+
+  def get(key: String, dest: File): Unit = {
     util.stream_to_file(client.getObject(bucket, key).getObjectContent(), dest)
   }
 
-  def put(src: File, bucket: String, key: String): Unit = {
+  def put(src: File, key: String): Unit = {
     client.putObject(bucket, key, src)
   }
 
-  def delete(bucket: String, key: String): Unit = {
+  def delete(key: String): Unit = {
     client.deleteObject(bucket, key)
   }
 }
