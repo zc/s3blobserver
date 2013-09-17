1d6155f63c391b6d1ef59f309ad0b952cd54ac07
Jim Fulton <jim@zope.com>
2013-09-17 18:45:18 -0400

    Added a main program that ties everything together, reading config info provided on the command line.
    Added default config.

---
+++ src/main/scala/main.scala
@@ -0,0 +1,50 @@
+package com.zope.s3blobserver
+
+import akka.actor.{ActorSystem, Props}
+import java.io.File
+import scala.concurrent.duration._
+import spray.can.Http
+import com.typesafe.config.ConfigFactory
+
+object Main extends App {
+  val name = "s3blobserver"
+
+  implicit val system = ActorSystem(
+    name+"-system",
+    ConfigFactory.load(ConfigFactory.parseFile(new File(args(0))))
+  )
+  implicit val dispatcher = system.dispatcher
+
+  val config = system.settings.config.getConfig(name)
+
+  val cache = S3BlobCache(
+    config.getBoolean("cache.same-file-system"),
+    new File(config.getString("cache.directory")),
+    config.getInt("cache.size"))
+
+  val s3 = new S3(S3.default_client, config.getString("s3.bucket"))
+
+  val committed = new File(config.getString("committed.directory"))
+
+  val service = system.actorOf(
+    Props(classOf[S3BlobServerActor], committed, cache, s3),
+    "s3blobserver")
+  val watcher = system.actorOf(
+    Props(classOf[Watcher],
+          committed,
+          config.getMilliseconds("committed.age").toInt,
+          cache, s3),
+    "watcher")
+
+  system.scheduler.schedule(
+    0.millisecond,
+    config.getMilliseconds(
+      "committed.poll-interval").toInt.millisecond,
+    watcher,
+    "")
+  akka.io.IO(Http) ! Http.Bind(
+    service,
+    config.getString("server.host"),
+    port = config.getInt("server.port")
+  )
+}
