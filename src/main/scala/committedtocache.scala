40c4cd34989f89c5adbab5067041278a396c2a13
Jim Fulton <jim@zope.com>
2013-10-10 11:48:54 -0400

    made the moving code resilient to having files removed before or during it's execution.  This could happen if the watcher scans for files before the results of the previous scan have been processed.

--- src/main/scala/committedtocache.scala
+++ src/main/scala/committedtocache.scala
@@ -1,3 +1,17 @@
+// Moving files from a committed directory to S3 and then to the cache.
+
+// A watcher actor is periodically woken up and scans the committed
+// directory for old .blob files. When it sees one, it sends it to the
+// worker pool.
+
+// Workers take care of sending the file to S3 and then passing it to
+// the cache.
+
+// It's possible that the watcher will run while the workers are still
+// processing the last scan, and that, as a result, a file previously
+// sent to the workers will be sent again.  A worker will then have to
+// process a file that doesn't exist.
+
 package com.zope.s3blobserver
 
 import akka.actor.{Actor, Props}
@@ -33,8 +47,16 @@ class MoveActor(cache: S3BlobCache, s3: S3) extends Actor {
 
   def receive = {
     case src: File ⇒
-      s3.put(src, src.getName)
-      cache.move(src)
+      if (src.exists)
+        try {
+          s3.put(src, src.getName)
+          cache.move(src)
+        }
+        catch {
+          case _: com.amazonaws.AmazonClientException =>
+          case _: java.io.FileNotFoundException =>
+          // The file went away because another mover handled it. <shrug>
+        }
   }
 }
 
--- src/test/scala/MoveSpec.scala
+++ src/test/scala/MoveSpec.scala
@@ -3,21 +3,84 @@ package com.zope.s3blobserver
 import akka.actor.ActorSystem
 import akka.testkit.TestActorRef
 import java.io.File
-import org.mockito.Mockito._
+import org.mockito.Mockito
 
-class MoveSpec extends org.scalatest.FlatSpec {
+class MoveSpec extends
+    org.scalatest.FlatSpec with
+    org.scalatest.BeforeAndAfter {
+
+  var dir: File = null
+
+  before {
+    dir = grizzled.file.util.createTemporaryDirectory("test")
+  }
+
+  after {
+    grizzled.file.util.deleteTree(dir)
+  }
 
   "A MoveActor" should "Move files" in {
 
-    val s3 = mock(classOf[S3])
-    val cache = mock(classOf[S3BlobCache])
+    val s3 = Mockito.mock(classOf[S3])
+    val cache = Mockito.mock(classOf[S3BlobCache])
     implicit val system = ActorSystem("test")
     val mover = TestActorRef(new MoveActor(cache, s3))
-    val f = new File("t")
+    val f = new File(dir, "t")
+    f.createNewFile()
     mover ! f
-    val inorder = inOrder(s3, cache)
+    val inorder = Mockito.inOrder(s3, cache)
+    inorder.verify(s3).put(f, "t")
+    inorder.verify(cache).move(f)
+    inorder.verifyNoMoreInteractions()
+
+  }
+
+  it should "not freak if a file isn't there" in {
+    val s3 = Mockito.mock(classOf[S3])
+    val cache = Mockito.mock(classOf[S3BlobCache])
+    implicit val system = ActorSystem("test")
+    val mover = TestActorRef(new MoveActor(cache, s3))
+    val f = new File(dir, "t")
+    mover ! f
+    val inorder = Mockito.inOrder(s3, cache)
+    inorder.verifyNoMoreInteractions()
+  }
+
+  it should "not freak when aws throws" in {
+    val s3 = Mockito.mock(classOf[S3])
+    val cache = Mockito.mock(classOf[S3BlobCache])
+    implicit val system = ActorSystem("test")
+    val mover = TestActorRef(new MoveActor(cache, s3))
+    val f = new File(dir, "t")
+    f.createNewFile()
+    Mockito.when(s3.put(f, "t")).thenThrow(
+      new com.amazonaws.AmazonClientException("test"))
+    mover ! f
+    val inorder = Mockito.inOrder(s3, cache)
+    inorder.verify(s3).put(f, "t")
+    inorder.verifyNoMoreInteractions()
+  }
+
+  it should "not freak when file can't be opened when copying to the cache" in {
+    val s3 = Mockito.mock(classOf[S3])
+    val cache = Mockito.mock(classOf[S3BlobCache])
+    implicit val system = ActorSystem("test")
+    val mover = TestActorRef(new MoveActor(cache, s3))
+    val f = new File(dir, "t")
+    f.createNewFile()
+
+    Mockito.doAnswer(
+      new org.mockito.stubbing.Answer[Unit]() {
+        def answer(invocation:
+                       org.mockito.invocation.InvocationOnMock): Unit =
+          throw new java.io.FileNotFoundException()
+      }).when(cache).move(f)
+
+    mover ! f
+    val inorder = Mockito.inOrder(s3, cache)
     inorder.verify(s3).put(f, "t")
     inorder.verify(cache).move(f)
     inorder.verifyNoMoreInteractions()
   }
+
 }
