afc2a72e8e4ab8b6a2fb23357f4f8040f3254a6b
Jim Fulton <jim@zope.com>
2013-10-28 13:23:18 -0400

    Separated code for invoking setup with config from code for making assertions, to make it sane to add a second test later (soon :).
    Also added an integration that 404s are logged.  We should add a more
    unity test of this in the server spec, but I needed to quiet an error
    to stdout anyway, so it was convenient to fill this test hole here.

--- src/test/scala/SetupSpec.scala
+++ src/test/scala/SetupSpec.scala
@@ -16,8 +16,11 @@ class SetupSpec extends
     org.scalatest.FlatSpec with
     Log4jTesting {
 
-  "main.Setup" should "Assemble the main process from config" in {
-
+  def with_config(
+    config: String
+  )(
+    func: (Setup, String, File, File, File, AmazonS3Client) => Unit
+  ) = {
     Testing.with_tmpdir {
       tmpdir =>
 
@@ -26,45 +29,15 @@ class SetupSpec extends
       val cachedir = new File(tmpdir, "cache")
       cachedir.mkdir
 
-      val config = """
-        akka.loglevel = ERROR
-        s3blobserver {
-          log4j = """"" + """"
-             log4j.rootLogger=WARN, OUT
-             log4j.appender.OUT=org.apache.log4j.ConsoleAppender
-             log4j.appender.OUT.layout=org.apache.log4j.PatternLayout
-             log4j.appender.OUT.layout.ConversionPattern=%-5p %c %m%n
-             """"" + s""""
-          cache {
-            same-file-system = true
-            directory = $cachedir
-            size = 100
-          }
-          s3 {
-            bucket = mybucket
-            prefix = test/
-          }
-          committed {
-            directory = $blobdir
-            age = 1s
-            poll-interval = 1s
-          }
-          server {
-            port = 0
-            host = localhost
-            path = /test
-            zookeeper = "zookeeper.example.com:2181"
-            zookeeper-data = somedata
-          }
-        }
-        """
-
       val config_file = new File(tmpdir, "test.conf")
-      util.stream_to_file(new ByteArrayInputStream(config.getBytes),
-                          config_file)
-
-      util.stream_to_file(new ByteArrayInputStream("blob1".getBytes),
-                          new File(blobdir, "1.blob"))
+      util.stream_to_file(
+        new ByteArrayInputStream(
+          config.
+            replace("$tmpdir", tmpdir.toString).
+            replace("$blobdir", blobdir.toString).
+            replace("$cachedir", cachedir.toString).
+            getBytes),
+        config_file)
 
       ProductionBindings.modifyBindings {
         implicit module =>
@@ -96,28 +69,6 @@ class SetupSpec extends
 
         // finally, set things up.
         val setup = new Setup(Array(config_file.toString))
-
-        // verify that the logging config was updated:
-        val logging_s = new java.io.ByteArrayOutputStream()
-        new org.apache.log4j.config.PropertyPrinter(
-          new java.io.PrintWriter(logging_s))
-        val logging = new java.util.Properties()
-        logging.load(new java.io.ByteArrayInputStream(logging_s.toByteArray))
-        expectResult("WARN, OUT") {
-          logging.getProperty("log4j.rootLogger")
-        }
-
-        // Set up another sample "blob" (that will stay in the blob directory)
-        util.stream_to_file(new ByteArrayInputStream("blob2".getBytes),
-                            new File(blobdir, "2"))
-
-        // Movers working:
-        val blobfile1 = 
-        Testing.wait_until("1.blob moved") {
-          new File(cachedir, "1.blob").exists
-        }
-        Mockito.verify(s3).putObject(
-          "mybucket", "test/1.blob", new File(blobdir, "1.blob"))
         
         // Get the server address via ZooKeeper registration
         Testing.wait_until("ZooKeeper client created") {
@@ -139,33 +90,105 @@ class SetupSpec extends
           })
         watcher.process(new WatchedEvent(null, SyncConnected, null))
 
-        // We can fetch 1.blob and 2
-        implicit val system = setup.system
-        import system.dispatcher
-        val client = sendReceive
-
-        expectResult("blob1") {
-          Testing.wait[HttpResponse](client(Get(base+"1.blob"))).entity.asString
-        }
-
-        expectResult("blob2") {
-          Testing.wait[HttpResponse](client(Get(base+"2"))).entity.asString
-        }
-        assert(new File(blobdir, "2").exists &&
-                 ! new File(cachedir, "2").exists)
-
-        // If we try to get a file that doesn't exist, we'll touch S3
-        Mockito.when(s3.getObject("mybucket", "test/3")).thenThrow(
-          new com.amazonaws.services.s3.model.AmazonS3Exception("nosuch"))
-        expectResult(404) {
-          Testing.wait[HttpResponse](client(Get(base+"3"))).status.intValue
-        }
-        Mockito.verify(s3).getObject("mybucket", "test/3")
+        func(setup, base, tmpdir, blobdir, cachedir, s3)
 
         // cleanup
-        system.shutdown()
+        setup.system.shutdown()
+      }
+    }
+  }
+
+  "main.Setup" should "Assemble the main process from config" in {
+    with_config("""
+        akka.loglevel = ERROR
+        s3blobserver {
+          log4j = """"" + """"
+             log4j.rootLogger=WARN, TEST
+             log4j.appender.TEST=org.apache.log4j.FileAppender
+             log4j.appender.TEST.File=$tmpdir/log
+             log4j.appender.TEST.layout=org.apache.log4j.PatternLayout
+             log4j.appender.TEST.layout.ConversionPattern=%-5p %c %m%n
+             """"" + """"
+          cache {
+            same-file-system = true
+            directory = $cachedir
+            size = 100
+          }
+          s3 {
+            bucket = mybucket
+            prefix = test/
+          }
+          committed {
+            directory = $blobdir
+            age = 1s
+            poll-interval = 1s
+          }
+          server {
+            port = 0
+            host = localhost
+            path = /test
+            zookeeper = "zookeeper.example.com:2181"
+            zookeeper-data = somedata
+          }
+        }
+        """
+    ) {
+      (setup, base, tmpdir, blobdir, cachedir, s3) =>
+
+      // verify that the logging config was updated:
+      val logging_s = new java.io.ByteArrayOutputStream()
+      new org.apache.log4j.config.PropertyPrinter(
+        new java.io.PrintWriter(logging_s))
+      val logging = new java.util.Properties()
+      logging.load(new java.io.ByteArrayInputStream(logging_s.toByteArray))
+      expectResult("WARN, TEST") {
+        logging.getProperty("log4j.rootLogger")
       }
 
+      // Movers working:
+      util.stream_to_file(new ByteArrayInputStream("blob1".getBytes),
+                          new File(blobdir, "1.blob"))
+      val blobfile1 =
+        Testing.wait_until("1.blob moved") {
+          new File(cachedir, "1.blob").exists
+        }
+      Mockito.verify(s3).putObject(
+        "mybucket", "test/1.blob", new File(blobdir, "1.blob"))
+
+      // Set up another sample "blob" (that will stay in the blob directory)
+      util.stream_to_file(new ByteArrayInputStream("blob2".getBytes),
+                          new File(blobdir, "2"))
+
+      // We can fetch 1.blob and 2
+      implicit val system = setup.system
+      import system.dispatcher
+      val client = sendReceive
+
+      expectResult("blob1") {
+        Testing.wait[HttpResponse](client(Get(base+"1.blob"))).entity.asString
+      }
+
+      expectResult("blob2") {
+        Testing.wait[HttpResponse](client(Get(base+"2"))).entity.asString
+      }
+      assert(new File(blobdir, "2").exists &&
+               ! new File(cachedir, "2").exists)
+
+      // If we try to get a file that doesn't exist, we'll touch S3
+      Mockito.when(s3.getObject("mybucket", "test/3")).thenThrow(
+        new com.amazonaws.services.s3.model.AmazonS3Exception("nosuch"))
+      expectResult(404) {
+        Testing.wait[HttpResponse](client(Get(base+"3"))).status.intValue
+      }
+      Mockito.verify(s3).getObject("mybucket", "test/3")
+
+      // The failure is logged
+      val buffer = new Array[Byte](999)
+      val read = new java.io.FileInputStream(new File(tmpdir, "log")).read(buffer)
+      val log = new String(buffer.slice(0, read)).split("\n")(0) // first line
+      assert(log.startsWith("ERROR"))
+      assert(log.containsSlice("/3"))
+      assert(log.endsWith(": wtf?"))
     }
   }
 }
